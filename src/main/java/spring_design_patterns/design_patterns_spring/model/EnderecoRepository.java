342fd8c49624211683e6de3a72f3a489acebdb8a
Fernanda Ishida <fer.ishida@gmail.com>
2024-07-18 23:00:28 -0300

    Design patterns com Java e Spring

---
+++ src/main/java/spring_design_patterns/design_patterns_spring/model/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package spring_design_patterns.design_patterns_spring.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
---
+++ src/main/java/spring_design_patterns/design_patterns_spring/service/ClienteService.java
@@ -0,0 +1,25 @@
+package spring_design_patterns.design_patterns_spring.service;
+
+
+import spring_design_patterns.design_patterns_spring.model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ *
+ * Instrutor falvojr
+ */
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
